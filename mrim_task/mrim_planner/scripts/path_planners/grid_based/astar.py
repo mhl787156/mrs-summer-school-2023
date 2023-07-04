4e50796d31a610d2a0c4eccfb4430b73e4098b41
mickey li <mickeyhli@outlook.com>
2023-07-04 14:55:13 +0100

    Simple Euclidean Astar heuristic implemented and tested some grid sizes

--- mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
+++ mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
@@ -72,7 +72,8 @@ class Node:
         b = self.pos[1] - self.goal[1]
         c = self.pos[2] - self.goal[2]
 
-        raise NotImplementedError('[STUDENTS TODO] Heuristic function guiding the state space exploration not implemented. You have to finish it on your own.')
+        return sqrt(a**2 + b**2 + c**2)
+        # raise NotImplementedError('[STUDENTS TODO] Heuristic function guiding the state space exploration not implemented. You have to finish it on your own.')
 # # #}
 
 # # #{ class AStar
